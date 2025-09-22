678589c60861a1e185120cb4d3674b42e26e00c9
ThanhLe86 <135790325+ThanhLe86@users.noreply.github.com>
2025-09-22 16:26:35 +0700

    feat: stable lerp value generation

--- textureGen.cpp
+++ textureGen.cpp
@@ -5,6 +5,7 @@ int	main(int argc, char **argv)
 {
     int image_height = 400;
     int image_width = 800;
+    uint32_t max_int = 4294967295;
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
@@ -16,16 +17,21 @@ int	main(int argc, char **argv)
             float x = (float)i * 0.05f; // important (*)
             float y = (float)j * 0.05f;
 
+            uint32_t x_floor = static_cast<uint32_t>(floor(x));
+            uint32_t x_ceil = static_cast<uint32_t>(ceil(x));
+            uint32_t y_floor = static_cast<uint32_t>(floor(y));
+            uint32_t y_ceil = static_cast<uint32_t>(ceil(y));
+
             //combine the numbers 
-            uint32_t n00 = hash(fixed_point_rep(x, y));
-            uint32_t n01 = hash(fixed_point_rep(x, y));
-            uint32_t n10 = hash(fixed_point_rep(x, y));
-            uint32_t n11 = hash(fixed_point_rep(x, y));
+            float n00 = (float)hash(combine(x_floor, y_floor)) / max_int;
+            float n01 = (float)hash(combine(x_floor, y_ceil)) / max_int;
+            float n10 = (float)hash(combine(x_ceil, y_floor))/ max_int;
+            float n11 = (float)hash(combine(x_ceil, y_ceil)) / max_int;
 
             //bilinear interpolation
-            float biline_lerp = lerp(lerp(n00, n01, x - floor(x)), lerp(n01, n11, x - floor(x)), y - floor(y));
+            float biline_lerp = lerp(lerp(n00, n01, x - x_floor), lerp(n01, n11, x - x_floor), y - y_floor);
 
-            std::cout << biline_lerp << ' ' << biline_lerp << ' ' << biline_lerp << '\n';
+            std::cout << biline_lerp << '\n';
         }
     }
 
